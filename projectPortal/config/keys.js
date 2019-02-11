0164a8ef80ad52e4ceaddbf823aa968f80c703f0
Saksham <sakshamr@protonmail.com>
2019-02-11 21:27:21 +0530

    Removed The Private Keys

--- projectPortal/config/keys.js
+++ projectPortal/config/keys.js
@@ -1,6 +1,6 @@
 
-const mongoUrl = 'mongodb://sakdev:sakdev5@ds227255.mlab.com:27255/campushack';
-const SendgridApiKey = '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';
+const mongoUrl = '';
+const SendgridApiKey = '';
 
 module.exports = {
     mongoUrl,
